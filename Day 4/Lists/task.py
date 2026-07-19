56ec225a89fc3608e8d2c439acb008838310f68e
Dhanusha B H <dhanushabh3@gmail.com>
2026-07-19 18:13:24 +0530

    Day-4:list,Randomaziation,Rock Paper Scissors Project

---
+++ Day 4/Lists/task.py
@@ -0,0 +1,10 @@
+states_of_india = ["Andra Pradesh" , "Arunchal Pradesh", "Bihar", "Goa","Karnataka" ]
+print(states_of_india[0])
+
+states_of_india[1] = "ArunchalPradesh"
+
+states_of_india.append("Kerala")
+
+states_of_india.extend(["Maharashtra","Mumbai"])
+
+print(states_of_india)
