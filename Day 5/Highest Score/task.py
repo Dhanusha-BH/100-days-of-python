6bdd126444f9bc3f2b02384ef37584447d940f64
Dhanusha B H <dhanushabh3@gmail.com>
2026-07-19 18:14:41 +0530

    Day-5:Loops,Password Generator Project

---
+++ Day 5/Highest Score/task.py
@@ -0,0 +1,7 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+
+maxi =0
+for max_score in student_scores:
+    if max_score > maxi:
+        maxi = max_score
+print(maxi)
