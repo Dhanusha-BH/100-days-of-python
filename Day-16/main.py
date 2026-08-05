0bf20ea5df7370ed6a60b881e62edcdefcfd2e9d
Dhanusha B H <dhanushabh3@gmail.com>
2026-08-05 10:35:57 +0530

    Day-16:OOP-classes and object

---
+++ Day-16/main.py
@@ -0,0 +1,15 @@
+import turtle
+from turtle import Turtle, Screen
+
+
+
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("blue")
+timmy.forward(100)
+
+myScreen = Screen()
+print(myScreen.canvheight)
+
+myScreen.exitonclick()
