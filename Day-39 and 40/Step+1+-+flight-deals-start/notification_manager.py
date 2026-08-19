7f72a85b2ce44e4b25e797fd793bc82cbad3ee33
Dhanusha B H <dhanushabh3@gmail.com>
2026-08-19 14:53:49 +0530

    Capstone Project:FlightClub

---
+++ Day-39 and 40/Step+1+-+flight-deals-start/notification_manager.py
@@ -0,0 +1,24 @@
+import smtplib
+import  os
+
+class NotificationManager:
+
+    def __init__(self):
+        self.smtp_address = os.environ["EMAIL_PROVIDER_SMTP_ADDRESS"]
+        self.email = os.environ.get("MY_EMAIL")
+        self.email_password = os.environ.get("MY_PASSWORD")
+        self.connection = smtplib.SMTP(os.environ["EMAIL_PROVIDER_SMTP_ADDRESS"],port=587)
+        print("Password loaded:", self.email_password)
+
+    def send_emails(self, email_list, email_body):
+        with self.connection:
+            self.connection.starttls()
+            self.connection.login(self.email, self.email_password)
+            for email in email_list:
+                self.connection.sendmail(
+                    from_addr=self.email,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{email_body}".encode('utf-8')
+                )
+
+
